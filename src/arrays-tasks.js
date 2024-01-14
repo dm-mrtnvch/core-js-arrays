5363a8a764048632ee020cbf829f8955625af40c
dm-mrtnvch <dmvshn@gmail.com>
2024-01-14 23:45:26 +0400

    feat: isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -145,8 +145,10 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length <= 1) return true;
+
+  return arr.every((el) => el.length === arr[0].length);
 }
 
 /**

64447c1525ffbcec47935ccbc6f91325de3cd42b
dm-mrtnvch <dmvshn@gmail.com>
2024-01-16 13:43:41 +0400

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -349,8 +349,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((acc, el, i) => {
+    const chunkIndex = Math.floor(i / chunkSize);
+
+    if (!acc[chunkIndex]) acc[chunkIndex] = [];
+
+    acc[chunkIndex].push(el);
+    return acc;
+  }, []);
 }
 
 /**
